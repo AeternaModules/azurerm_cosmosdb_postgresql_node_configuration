16727c68c29d27243c44a11b1a6383fbb64e9e5a
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:37 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,13 @@
-output "cosmosdb_postgresql_node_configurations" {
-  description = "All cosmosdb_postgresql_node_configuration resources"
-  value       = azurerm_cosmosdb_postgresql_node_configuration.cosmosdb_postgresql_node_configurations
-}
 output "cosmosdb_postgresql_node_configurations_cluster_id" {
-  description = "List of cluster_id values across all cosmosdb_postgresql_node_configurations"
-  value       = [for k, v in azurerm_cosmosdb_postgresql_node_configuration.cosmosdb_postgresql_node_configurations : v.cluster_id]
+  description = "Map of cluster_id values across all cosmosdb_postgresql_node_configurations, keyed the same as var.cosmosdb_postgresql_node_configurations"
+  value       = { for k, v in azurerm_cosmosdb_postgresql_node_configuration.cosmosdb_postgresql_node_configurations : k => v.cluster_id }
 }
 output "cosmosdb_postgresql_node_configurations_name" {
-  description = "List of name values across all cosmosdb_postgresql_node_configurations"
-  value       = [for k, v in azurerm_cosmosdb_postgresql_node_configuration.cosmosdb_postgresql_node_configurations : v.name]
+  description = "Map of name values across all cosmosdb_postgresql_node_configurations, keyed the same as var.cosmosdb_postgresql_node_configurations"
+  value       = { for k, v in azurerm_cosmosdb_postgresql_node_configuration.cosmosdb_postgresql_node_configurations : k => v.name }
 }
 output "cosmosdb_postgresql_node_configurations_value" {
-  description = "List of value values across all cosmosdb_postgresql_node_configurations"
-  value       = [for k, v in azurerm_cosmosdb_postgresql_node_configuration.cosmosdb_postgresql_node_configurations : v.value]
+  description = "Map of value values across all cosmosdb_postgresql_node_configurations, keyed the same as var.cosmosdb_postgresql_node_configurations"
+  value       = { for k, v in azurerm_cosmosdb_postgresql_node_configuration.cosmosdb_postgresql_node_configurations : k => v.value }
 }
 
